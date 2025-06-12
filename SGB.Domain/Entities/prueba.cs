02f1408144304374a99d7e3d3786c21cedb899c5
IsmaelTejed <ismael261126@gmail.com>
2025-06-12 19:54:44 -0400

    prueba

---
+++ SGB.Domain/Entities/prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGB.Domain.Entities
+{
+    public class prueba
+    {
+    }
+}
