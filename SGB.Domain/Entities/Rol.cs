8c1aeba3efb38fa99398a1d261d51136a06d38c8
Helbert Garcia <helbert1001@gmail.com>
2025-06-12 19:16:53 -0400

    Creando la entidad Rol en la capa de entidades

---
+++ SGB.Domain/Entities/Rol.cs
@@ -0,0 +1,42 @@
+﻿using SGB.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGB.Domain.Entities
+{
+    public class Rol: BaseEntity
+    {
+        public string Nombre { get; private set; }
+        public string Descripcion { get; private set; }
+
+        private Rol() : base() { }
+
+        public Rol(string nombre, string descripcion = "") : base()
+        {
+            ValidarYAsignarNombre(nombre);
+            Descripcion = descripcion;
+        }
+
+        public void ActualizarDescripcion(string nuevaDescripcion)
+        {
+            Descripcion = nuevaDescripcion;
+        }
+
+        public void CambiarNombre(string nuevoNombre)
+        {
+            ValidarYAsignarNombre(nuevoNombre);
+        }
+
+        private void ValidarYAsignarNombre(string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                throw new ArgumentException("El nombre del rol es obligatorio.", nameof(nombre));
+            }
+            Nombre = nombre;
+        }
+    }
+}
