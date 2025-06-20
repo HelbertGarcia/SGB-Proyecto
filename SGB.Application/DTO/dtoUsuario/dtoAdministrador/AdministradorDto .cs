cd5b377f4801dd1b552cbfa93afb417a43507469
Rashel <rashelroadiaz005@gmail.com>
2025-06-20 16:14:29 -0200

    Agregando carpeta: dtoAdministrador en capa aplicacion, renombrando Class1 a DtoBase, Creando interfaces IEstaActivo e IFechaActualizacion

---
+++ SGB.Application/DTO/dtoUsuario/dtoAdministrador/AdministradorDto .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGB.Persistence.Interfaces;
+
+namespace SGB.Application.DTO.dtoUsuario.dtoAdministrador
+{
+    public class AdministradorDto : IFechaActualizacion, IEstaActivo
+    {
+        public int IDAdmin { get; set; }
+        public int IDUsuario { get; set; }
+        public DateTime FechaAsignacion { get; set; }
+        public bool EstaActivo { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+
+
+
+    }
+}
--- SGB.Application/DtoBase.cs
+++ SGB.Application/DtoBase.cs
@@ -1,6 +1,6 @@
 ﻿namespace SGB.Application
 {
-    public class Class1
+    public class DtoBase
     {
 
     }
