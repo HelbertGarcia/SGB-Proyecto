222dd90ec3eb207f6a896e8666e726f0235d5dac
Eithan <eithanread1@gmail.com>
2025-06-20 13:53:29 -0400

    cambios en la estrutura dto

---
+++ SGB.Application/DTO/Prestamo y Penalizacion/PenalizacionDto/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGB.Application.DTO.Prestamo_y_Penalizacion.PenalizacionDto
+{
+    internal class Class1
+    {
+    }
+}
