26609ae1c17f2df96942f8a24c536623472650a3
Helbert Garcia <helbert1001@gmail.com>
2025-06-26 15:53:29 -0400

    creando carpetas para los servicios

---
+++ SGB.Application/Base/IBaseService.cs
@@ -0,0 +1,20 @@
+﻿using SGB.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGB.Application.Base
+{
+    public interface IBaseService
+    {
+        Task<OperationResult> AddAsync<T>(T entity) where T : class;
+
+        Task<OperationResult> UpdateAsync<T>(T entity) where T : class;
+
+        Task<OperationResult> DeleteAsync<T>(int id) where T : class;
+
+        Task<T> GetByIdAsync<T>(T id) where T : class;
+    }
+}
