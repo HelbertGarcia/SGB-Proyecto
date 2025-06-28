8b264306901590ddd2c37324358ef499d93e2735
Rashel <rashelroadiaz005@gmail.com>
2025-06-28 09:26:03 -0200

    Ir guardando cambios: Capa Core Domain Entities = Configuracion .cs, SGBContext, Models = ConfiguracionModels, Capa Infraestructura Persistencia: ConfiguracionRepository , IConfiguracionRepository

---
+++ SGB.Persistence/Interfaces/IConfiguracionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGB.Domain.Base;
+
+namespace SGB.Persistence.Interfaces
+{
+    public interface IConfiguracionRepository
+    {
+        Task<OperationResult> ObtenerPorNombreAsync(string nombre);
+    }
+}
