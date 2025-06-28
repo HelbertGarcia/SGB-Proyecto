5297cf7607114c532ad0e4640afddc4365229972
Helbert Garcia <helbert1001@gmail.com>
2025-06-28 10:15:56 -0400

    confirmar

---
+++ SGB.Application/Contratos/Servicios/Libros/LibroService.cs
@@ -0,0 +1,37 @@
+﻿using SGB.Application.Contratos.interfaces.LibrosServices;
+using SGB.Application.Dtos.LibrosDto.LibroDto;
+using SGB.Domain.Base;
+using SGB.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGB.Application.Contratos.Servicios.Libros
+{
+    public class LibroService : ILibroService
+    {
+        private readonly LibroRepository _libroRepository;
+
+        public Task<OperationResult> AddAsync<T>(T entity) where T : class
+        {
+            _libroRepository.AddAsync();
+        }
+
+        public Task<OperationResult> DeleteAsync<T>(int id) where T : class
+        {
+
+        }
+
+        public Task<T> GetByIdAsync<T>(T id) where T : class
+        {
+
+        }
+
+        public Task<OperationResult> UpdateAsync<T>(T entity) where T : class
+        {
+
+        }
+    }
+}
